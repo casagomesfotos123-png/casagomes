6aff1411221efbf467399190b795a7ceb9bc3099
mateus celestno <mateusnascimentocelestino@gmail.com>
2025-09-30 01:47:13 -0300

    ajetando url do back

--- casa_gomes/service/api.js
+++ casa_gomes/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000", // backend
+  baseURL: "https://casagomes.onrender.com", // backend
 });
 
 // 🔑 Interceptor: adiciona token JWT em cada requisição
